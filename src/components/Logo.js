dc9a8043b5ae534d9a0d0f2304ae349f7ce6d79a
JamesLyddon <jameslyddon@gmail.com>
2022-04-16 12:11:29 +0100

    Update Logo.js
    updated logo href

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,7 +3,7 @@ import { StyledLogo } from "./styles/Logo.styled";
 export default function Logo() {
   return (
     <StyledLogo>
-      <a href="/">
+      <a href="https://jameslyddon.github.io/joli">
         <img
           className="logo-img"
           src={require("../assets/images/joli.jpg")}
